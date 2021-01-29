4be3a515e2a67d796711b95359366b0cb9d36362
NVV <uniondirfolder@gmail.com>
2021-01-29 05:31:30 -0800

    Configure Presentation Layer for Dependency Container

--- CleanArch/CleanArch.Mvc/Startup.cs
+++ CleanArch/CleanArch.Mvc/Startup.cs
@@ -1,4 +1,5 @@
 using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
 using CleanArch.Mvc.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -42,6 +43,8 @@ namespace CleanArch.Mvc
             {
                 options.UseSqlServer(Configuration.GetConnectionString("UniversityDBConnection"));
             });
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,6 +76,13 @@ namespace CleanArch.Mvc
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+
+
+        //my music
+        private static void RegisterServices(IServiceCollection services) 
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
