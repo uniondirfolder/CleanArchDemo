29bf77b10d5229c73a63dfb75a6739dd28f6f6ad
NVV <uniondirfolder@gmail.com>
2021-01-29 04:13:41 -0800

    Commit Service and Repository Interfaces, CourseViewModels

---
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿
+using CleanArch.Application.ViewModels;
+using System.Collections.Generic;
+
+
+namespace CleanArch.Application.Interfaces
+{
+    interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
---
+++ CleanArch/CleanArch.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,11 @@
+﻿using CleanArch.Domain.Models;
+using System.Collections.Generic;
+
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
