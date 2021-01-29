80bb88a253ff4f7aae6bf049ede9a27312345f93
NVV <uniondirfolder@gmail.com>
2021-01-29 11:47:27 -0800

    Add Api Project and Rename Controller

---
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+
+        private readonly ILogger<CourseController> _logger;
+
+        public CourseController(ILogger<CourseController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+    }
+}
