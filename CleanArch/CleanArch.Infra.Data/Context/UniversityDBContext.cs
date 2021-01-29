dc703dc99516561a607c3294106916967c2e1f9e
NVV <uniondirfolder@gmail.com>
2021-01-29 03:29:33 -0800

    Initial Commit of Infrastructure.Data Project, Update MVC for new UniversityDBContext

---
+++ CleanArch/CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,16 @@
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UniversityDBContext:DbContext
+    {
+        public UniversityDBContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
